d8ec497307968bfabbc391cd3c5ced20ae0a688d
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2018-07-24 06:10:39 +0530

    Home Activity updated

--- app/src/main/java/com/example/rakesh/sensordata/Home.java
+++ app/src/main/java/com/example/rakesh/sensordata/Home.java
@@ -1,6 +1,7 @@
 package com.example.rakesh.sensordata;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,15 +9,63 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Home extends AppCompatActivity {
 
     private Toolbar hometoolbar;
     private FirebaseAuth mAuth;
     private FloatingActionButton addpost;
+    private FirebaseFirestore firebaseFirestore;
+    private String currentuser_id;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user == null){
+
+            sendToLogin();
+
+        }else{
+
+            currentuser_id = mAuth.getCurrentUser().getUid();
+            firebaseFirestore.collection("Users").document(currentuser_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()){
+                        if(!task.getResult().exists()){
+                            Intent setupintent = new Intent(Home.this,SetUpSettings.class);
+                            startActivity(setupintent);
+                            finish();
+                        }
+                    }else{
+
+
+                        Toast.makeText(Home.this, ""+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void sendToLogin() {
+        Intent intent = new Intent(Home.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +73,10 @@ public class Home extends AppCompatActivity {
 
         hometoolbar = (Toolbar)findViewById(R.id.home_toolbar);
         mAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+
+
         addpost = findViewById(R.id.addPostBtn);
 
         addpost.setOnClickListener(new View.OnClickListener() {
